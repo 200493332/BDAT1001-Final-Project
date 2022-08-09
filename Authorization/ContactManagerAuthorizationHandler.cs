2c1f5b811bf25113924f082718530ac156d53a81
200493332 <106766308+200493332@users.noreply.github.com>
2022-08-09 00:00:01 -0400

    Add files via upload

---
+++ Authorization/ContactManagerAuthorizationHandler.cs
@@ -0,0 +1,67 @@
+﻿using System.Threading.Tasks;
+using ContactManager.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authorization.Infrastructure;
+using Microsoft.AspNetCore.Identity;
+
+namespace ContactManager.Authorization
+{
+    public static class ContactOperations
+    {
+        public static OperationAuthorizationRequirement Create =
+          new OperationAuthorizationRequirement { Name = Constants.CreateOperationName };
+        public static OperationAuthorizationRequirement Read =
+          new OperationAuthorizationRequirement { Name = Constants.ReadOperationName };
+        public static OperationAuthorizationRequirement Update =
+          new OperationAuthorizationRequirement { Name = Constants.UpdateOperationName };
+        public static OperationAuthorizationRequirement Delete =
+          new OperationAuthorizationRequirement { Name = Constants.DeleteOperationName };
+        public static OperationAuthorizationRequirement Approve =
+          new OperationAuthorizationRequirement { Name = Constants.ApproveOperationName };
+        public static OperationAuthorizationRequirement Reject =
+          new OperationAuthorizationRequirement { Name = Constants.RejectOperationName };
+    }
+
+    public class Constants
+    {
+        public static readonly string CreateOperationName = "Create";
+        public static readonly string ReadOperationName = "Read";
+        public static readonly string UpdateOperationName = "Update";
+        public static readonly string DeleteOperationName = "Delete";
+        public static readonly string ApproveOperationName = "Approve";
+        public static readonly string RejectOperationName = "Reject";
+
+        public static readonly string ContactAdministratorsRole =
+                                                              "ContactAdministrators";
+        public static readonly string ContactManagersRole = "ContactManagers";
+    }
+    public class ContactManagerAuthorizationHandler :
+        AuthorizationHandler<OperationAuthorizationRequirement, Contact>
+    {
+        protected override Task
+            HandleRequirementAsync(AuthorizationHandlerContext context,
+                                   OperationAuthorizationRequirement requirement,
+                                   Contact resource)
+        {
+            if (context.User == null || resource == null)
+            {
+                return Task.CompletedTask;
+            }
+
+            // If not asking for approval/reject, return.
+            if (requirement.Name != Constants.ApproveOperationName &&
+                requirement.Name != Constants.RejectOperationName)
+            {
+                return Task.CompletedTask;
+            }
+
+            // Managers can approve or reject.
+            if (context.User.IsInRole(Constants.ContactManagersRole))
+            {
+                context.Succeed(requirement);
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
